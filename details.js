7b004c0d1b0ea739c6b8c9f4a89de2410c09c298
12roshan12 <roshansharma7501@gmail.com>
2022-12-09 13:57:06 +0545

    ch

--- details.js
+++ details.js
@@ -34,40 +34,78 @@ function sort(e) {
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  const thead = document.querySelector("#table1 thead");
-  const tbody = document.querySelector("#table1 tbody");
-  const tfoot = document.querySelector("#table1 tfoot");
-  const footerDiv = document.querySelector("#footerDiv");
-
-  const mappedData =fetchedData.filter((e) => e.id <= 5)
-  const tempData = JSON.parse(JSON.stringify(mappedData))
-  thead.innerHTML = `<tr>
-        ${Object.keys(tempData[0]).map(key => {
-    if (key != 'id') {
-      return `<th>${key.toUpperCase()}</th>`
+  if (window.screen.width <= 700) {
+    const thead = document.querySelector("#table1 thead");
+    const tbody = document.querySelector("#table1 tbody");
+    const tfoot = document.querySelector("#table1 tfoot");
+    const footerDiv = document.querySelector("#footerDiv");
+  
+    const mappedData =fetchedData.filter((e) => e.id <= 5)
+    const tempData = JSON.parse(JSON.stringify(mappedData))
+    thead.innerHTML = `<tr>
+          ${Object.keys(tempData[0]).map(key => {
+      if (key != 'id' && key != 'postalCode') {
+        return `<th>${key.toUpperCase()}</th>`
+      }
+    }).join('')}
+        </tr>`;
+    tbody.innerHTML = tempData
+      .map(item => {
+        delete item.id
+        delete item.postalCode
+        return `<tr>
+          ${Object.values(item).map(val => {
+          if ((typeof val) == 'number') {
+            return `<td  style="display:flex;justify-content:end" >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+          else {
+            return `<td   >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+        }).join('')}
+        </tr>`
+      }).join('')
+    const totallength = Math.ceil(fetchedData.length / 5)
+    const totallengtharray = []
+    for (let i = 1; i <= totallength; i++) {
+      totallengtharray.push(i)
     }
-  }).join('')}
-      </tr>`;
-  tbody.innerHTML = tempData
-    .map(item => {
-      delete item.id
-      return `<tr>
-        ${Object.values(item).map(val => {
-        if ((typeof val) == 'number') {
-          return `<td  style="display:flex;justify-content:end" >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
-        }
-        else {
-          return `<td   >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
-        }
-      }).join('')}
-      </tr>`
-    }).join('')
-  const totallength = Math.ceil(fetchedData.length / 5)
-  const totallengtharray = []
-  for (let i = 1; i <= totallength; i++) {
-    totallengtharray.push(i)
+    footerDiv.innerHTML = totallengtharray.map(e => `<div style="padding:2px;border:1px solid black;background:whitesmoke;margin:2px;cursor:pointer;border-radius:2px;box-shadow: 0 8px 6px -6px black;" id=\"clicked\" onclick=\"sort(${e})\">${e}</div>`).join('')
   }
-  footerDiv.innerHTML = totallengtharray.map(e => `<div style="padding:2px;border:1px solid black;background:whitesmoke;margin:2px;cursor:pointer;border-radius:2px;box-shadow: 0 8px 6px -6px black;" id=\"clicked\" onclick=\"sort(${e})\">${e}</div>`).join('')
+  else {
+    const thead = document.querySelector("#table1 thead");
+    const tbody = document.querySelector("#table1 tbody");
+    const tfoot = document.querySelector("#table1 tfoot");
+    const footerDiv = document.querySelector("#footerDiv");
+    const mappedData =fetchedData.filter((e) => e.id <= 5)
+    const tempData = JSON.parse(JSON.stringify(mappedData))
+    thead.innerHTML = `<tr>
+          ${Object.keys(tempData[0]).map(key => {
+      if (key != 'id' ) {
+        return `<th>${key.toUpperCase()}</th>`
+      }
+    }).join('')}
+        </tr>`;
+    tbody.innerHTML = tempData
+      .map(item => {
+        delete item.id
+        return `<tr>
+          ${Object.values(item).map(val => {
+          if ((typeof val) == 'number') {
+            return `<td  style="display:flex;justify-content:end" >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+          else {
+            return `<td   >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+        }).join('')}
+        </tr>`
+      }).join('')
+    const totallength = Math.ceil(fetchedData.length / 5)
+    const totallengtharray = []
+    for (let i = 1; i <= totallength; i++) {
+      totallengtharray.push(i)
+    }
+    footerDiv.innerHTML = totallengtharray.map(e => `<div style="padding:2px;border:1px solid black;background:whitesmoke;margin:2px;cursor:pointer;border-radius:2px;box-shadow: 0 8px 6px -6px black;" id=\"clicked\" onclick=\"sort(${e})\">${e}</div>`).join('')
+  }
 })
 
 const element = document.getElementById("search");
