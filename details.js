c5122f63020d79ee41997b7d36f73566243d981d
12roshan12 <roshansharma7501@gmail.com>
2022-12-09 14:06:56 +0545

    ch

--- details.js
+++ details.js
@@ -1,34 +1,99 @@
 // import data from './dummy.json' assert {type: 'json'};
 
+
 const fetchedData = data.data
 
 function sort(e) {
   const receivedIndex = e
-  const thead = document.querySelector("#table1 thead");
-  const tbody = document.querySelector("#table1 tbody");
-  const tfoot = document.querySelector("#table1 tfoot");
-  const footerDiv = document.querySelector("#footerDiv");
-  const totallength = Math.ceil(fetchedData.length / 5)
-  const mappedData = fetchedData.filter((e) => {
-    if (e.id > ((receivedIndex - 1) * 5) && e.id <= (receivedIndex * 5)) {
-      return e
-    }
-    else {
-    }
-  })
+  if (window.screen.width <= 700) {
+    const thead = document.querySelector("#table1 thead");
+    const tbody = document.querySelector("#table1 tbody");
+    const tfoot = document.querySelector("#table1 tfoot");
+    const footerDiv = document.querySelector("#footerDiv");
+    const totallength = Math.ceil(fetchedData.length / 5)
+    const mappedData = fetchedData.filter((e) => {
+      if (e.id > ((receivedIndex - 1) * 5) && e.id <= (receivedIndex * 5)) {
+        return e
+      }
+      else {
+      }
+    })
+    const tempData = JSON.parse(JSON.stringify(mappedData))
 
-  thead.innerHTML = `<tr>
-        ${Object.keys(mappedData[0]).map(key => `<th>${key.toUpperCase()}</th>`).join('')}
-      </tr>`;
-  tbody.innerHTML = mappedData
-    .map(item => `<tr>
-        ${Object.values(item).map(val => `<td>${val.toUpperCase()}</td>`).join('')}
-      </tr>`).join('')
-  const totallengtharray = []
-  for (let i = 1; i <= totallength; i++) {
-    totallengtharray.push(i)
+
+    thead.innerHTML = `<tr>
+          ${Object.keys(tempData[0]).map(key => {
+      if (key != 'id' && key != 'postalCode') {
+        return `<th>${key.toUpperCase()}</th>`
+      }
+    }).join('')}
+        </tr>`;
+    tbody.innerHTML = tempData
+      .map(item => {
+        delete item.id
+        delete item.postalCode
+        return `<tr>
+          ${Object.values(item).map(val => {
+          if ((typeof val) == 'number') {
+            return `<td  style="display:flex;justify-content:end" >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+          else {
+            return `<td   >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+        }).join('')}
+        </tr>`
+      }).join('')
+    // const totallength = Math.ceil(fetchedData.length / 5)
+    const totallengtharray = []
+    for (let i = 1; i <= totallength; i++) {
+      totallengtharray.push(i)
+    }
+    footerDiv.innerHTML = totallengtharray.map(e => `<div style="padding:2px;border:1px solid black;background:whitesmoke;margin:2px;cursor:pointer;border-radius:2px;box-shadow: 0 8px 6px -6px black;" id=\"clicked\" onclick=\"sort(${e})\">${e}</div>`).join('')
   }
-  footerDiv.innerHTML = totallengtharray.map(e => `<div style="padding:2px;border:1px solid black;background:whitesmoke;margin:2px;cursor:pointer;border-radius:2px;box-shadow: 0 8px 6px -6px black;" id=\"clicked\" onclick=\"sort(${e})\">${e}</div>`).join('')
+  else {
+    const thead = document.querySelector("#table1 thead");
+    const tbody = document.querySelector("#table1 tbody");
+    const tfoot = document.querySelector("#table1 tfoot");
+    const footerDiv = document.querySelector("#footerDiv");
+    // const totallength = Math.ceil(fetchedData.length / 5)
+    const mappedData = fetchedData.filter((e) => {
+      if (e.id > ((receivedIndex - 1) * 5) && e.id <= (receivedIndex * 5)) {
+        return e
+      }
+      else {
+      }
+    })
+    const tempData = JSON.parse(JSON.stringify(mappedData))
+
+
+    thead.innerHTML = `<tr>
+          ${Object.keys(tempData[0]).map(key => {
+      if (key != 'id') {
+        return `<th>${key.toUpperCase()}</th>`
+      }
+    }).join('')}
+        </tr>`;
+    tbody.innerHTML = tempData
+      .map(item => {
+        delete item.id
+        return `<tr>
+          ${Object.values(item).map(val => {
+          if ((typeof val) == 'number') {
+            return `<td  style="display:flex;justify-content:end" >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+          else {
+            return `<td   >${(typeof val) == 'number' ? val : val.toUpperCase()}</td>`
+          }
+        }).join('')}
+        </tr>`
+      }).join('')
+    const totallength = Math.ceil(fetchedData.length / 5)
+    const totallengtharray = []
+    for (let i = 1; i <= totallength; i++) {
+      totallengtharray.push(i)
+    }
+    footerDiv.innerHTML = totallengtharray.map(e => `<div style="padding:2px;border:1px solid black;background:whitesmoke;margin:2px;cursor:pointer;border-radius:2px;box-shadow: 0 8px 6px -6px black;" id=\"clicked\" onclick=\"sort(${e})\">${e}</div>`).join('')
+  }
 
 }
 
@@ -39,8 +104,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const tbody = document.querySelector("#table1 tbody");
     const tfoot = document.querySelector("#table1 tfoot");
     const footerDiv = document.querySelector("#footerDiv");
-  
-    const mappedData =fetchedData.filter((e) => e.id <= 5)
+
+    const mappedData = fetchedData.filter((e) => e.id <= 5)
     const tempData = JSON.parse(JSON.stringify(mappedData))
     thead.innerHTML = `<tr>
           ${Object.keys(tempData[0]).map(key => {
@@ -76,11 +141,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const tbody = document.querySelector("#table1 tbody");
     const tfoot = document.querySelector("#table1 tfoot");
     const footerDiv = document.querySelector("#footerDiv");
-    const mappedData =fetchedData.filter((e) => e.id <= 5)
+    const mappedData = fetchedData.filter((e) => e.id <= 5)
     const tempData = JSON.parse(JSON.stringify(mappedData))
     thead.innerHTML = `<tr>
           ${Object.keys(tempData[0]).map(key => {
-      if (key != 'id' ) {
+      if (key != 'id') {
         return `<th>${key.toUpperCase()}</th>`
       }
     }).join('')}
@@ -168,8 +233,8 @@ window.addEventListener('resize', function (e) {
     const tbody = document.querySelector("#table1 tbody");
     const tfoot = document.querySelector("#table1 tfoot");
     const footerDiv = document.querySelector("#footerDiv");
-  
-    const mappedData =fetchedData.filter((e) => e.id <= 5)
+
+    const mappedData = fetchedData.filter((e) => e.id <= 5)
     const tempData = JSON.parse(JSON.stringify(mappedData))
     thead.innerHTML = `<tr>
           ${Object.keys(tempData[0]).map(key => {
@@ -205,11 +270,11 @@ window.addEventListener('resize', function (e) {
     const tbody = document.querySelector("#table1 tbody");
     const tfoot = document.querySelector("#table1 tfoot");
     const footerDiv = document.querySelector("#footerDiv");
-    const mappedData =fetchedData.filter((e) => e.id <= 5)
+    const mappedData = fetchedData.filter((e) => e.id <= 5)
     const tempData = JSON.parse(JSON.stringify(mappedData))
     thead.innerHTML = `<tr>
           ${Object.keys(tempData[0]).map(key => {
-      if (key != 'id' ) {
+      if (key != 'id') {
         return `<th>${key.toUpperCase()}</th>`
       }
     }).join('')}
